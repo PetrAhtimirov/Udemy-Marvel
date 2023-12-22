64e95833d0d11c3fbb3b4eeaede6ce385677b79c
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-23 02:38:54 +0300

    create random character component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,32 @@ class MarvelServise {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        let desc = char.description;
+        if (desc === '' ) {
+            desc = 'Данные о персонаже отсутствуют.';
+        }
+        if (desc.length > 160) {
+            desc = `${desc.substr(0, 160)}...`;
+        } 
+
+        return {
+            name: char.name,
+            description: desc,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
