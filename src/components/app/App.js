5549bc371ca8971491bf15a182d6330f7be51173
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-25 00:54:34 +0300

    delete unnecessary images

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,8 +6,6 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-import decoration from '../../resources/img/vision.png';
-
 class App extends Component {
     state = {
         selectedChar: null
@@ -35,7 +33,6 @@ class App extends Component {
                             <CharInfo charId={this.state.selectedChar}/>
                         </ErrorBoundary>
                     </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
             </div>
         )
